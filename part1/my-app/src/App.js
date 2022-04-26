71308537d21c8089ab161e9d30cffe5d102314bb
James Tang <jamestang@uvic.ca>
2022-04-25 23:53:03 -0700

    anecdote part1 change

--- part1/my-app/src/App.js
+++ part1/my-app/src/App.js
@@ -1,99 +1,87 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css'
+// import './App.css'
 
-const Statistics = (props) => {
-  if (props.allClicks.length == 0){
-    return(
+
+const MostVotes = (props) =>{
+  // console.log(props.t1)
+  // console.log(props.t2)
+  if (props.check == false){
+    return (
       <div>
-        <h1>{props.text}</h1>
-        <p>No feedback given</p>
+        <h1>Anecdote with most votes</h1>
+        <p>No vote has been processed.</p>
       </div>
     )
   }
-  
-  return(
+  return (
     <div>
-      <h1>{props.text}</h1>
-      <table>
-        <tbody>
-        <Statisticsline text = {'good'} value={props.good} />
-        <Statisticsline text = {'neutral'} value={props.neutral} />
-        <Statisticsline text = {'bad'} value={props.bad} />
-        <Statisticsline text = {'positive'} value={props.positive} />
-        </tbody>
-      </table>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.quote}</p>
+      <p>has {props.vote} votes</p>
     </div>
-    )
-
-}
-
-const Statisticsline = (props) => {
-  return(
-      <tr>
-        <th>{props.text}: </th>
-        <th>{props.value}</th>
-      </tr>
-    // <div>{props.text}: {props.value}</div>
-  )
-}
-
-const Button = ({handleClick, text}) => {
-  return(
-    <button onClick={handleClick}>{text}</button>
   )
 }
 
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([])
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
 
-  const handleGood = (props) => {
-    setAll(allClicks.concat('G'))
-    console.log("Good")
-    setGood(good + 1)
+
+  //Difference between using useState or directly setting up points array?
+  // const [points, setPoints] = useState([])
+
+  const n = anecdotes.length
+  // points = Array(n).fill(0)
+  // console.log(points)
+  const index = Math.round((Math.random()*anecdotes.length-1)+0)
+  const [points, setPoints] = useState(Array(n).fill(0))
+  const [selected, setSelected] = useState(0)
+  const [voteCheck, setVoteCheck] = useState(false)
+  const [popular, setPopular] = useState(0)
+  // const popular = 0
+
+
+  const handleSelect = (props) => {
+    // Setting selected variable to be the value returned by the randomized index (concrete value, not random)
+    console.log("I am here")
+    setSelected(index)
   }
 
-  const handleNeutral= (props) => {
-    setAll(allClicks.concat('N'))
-    setNeutral(neutral + 1)
+  const handleVote = (props) => {
+    // points[index] += 1
+    const copy = [...points]
+    // console.log(copy)
+    copy[selected] += 1
+    setPoints(copy)
+    // Interesting behaviour happening here, it seems like setPoints isn't updating points in real time, causing popular to lag behind by 1
+    // console.log(copy)
+    // console.log(points)
+    // console.log(Math.max(...points))
+    setVoteCheck(true)
+    setPopular(Math.max(...copy))
   }
-
-  const handleBad = (props) => {
-    setAll(allClicks.concat('B'))
-    setBad(bad + 1)
-  }
-  // const [average, setAverage] = useState(0)
-  // handleGoodClick
-  // handleNeutralClilck
-  // handleBadClick
-  const positive = (good/(good+neutral+bad))*100 + "%"
 
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <Button handleClick={handleGood} text='Good' />
-      <Button handleClick={handleNeutral} text='Neutral' />
-      <Button handleClick={handleBad} text='Bad' />
-      {/* <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutral}>Neutral</button>
-      <button onClick={handleBad}>Bad</button> */}
+      <div>
+          <h1>Anecdote of the day</h1>
+          <p>{anecdotes[selected]}</p>
+          <p>has {points[selected]} votes</p>
+          {/* Why does the buttons below not go ontop of each other instead it goes side by side */}
+          <button onClick={handleSelect}>next anecdote</button>
+          <button onClick={handleVote}>vote</button>
 
-      <Statistics text='Statistics' good={good} neutral={neutral} bad={bad} positive={positive} allClicks={allClicks}/>
-
-
-      {/* <h1>Statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good + neutral + bad}</p>
-      {/* <p>average: {}</p> */}
-      {/* <p>positive: {positive}</p> */}
-    </div>
+          <MostVotes vote={popular} check = {voteCheck} quote={anecdotes[points.indexOf(popular)]} points={popular} t1={points} t2={popular}/>
+      </div>
   )
 }
 export default App;
