8f58dfa3a283668e280679446d96d8ec407a5bca
James Tang <52274101+ganmany5288@users.noreply.github.com>
2022-12-29 22:56:56 -0800

    Update App.js
    Exercise 2.9:
    - added 2 useState functions to assist with filtering
    - had the initial state of filterAll to be persons (might break)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', phoneNumber: '123-4567-890', id: 1}
+    { name: 'Arto Hellas', phoneNumber: '123-4567-890', id: 1},
+    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [filterAll, setFilterAll] = useState('')
 
+  const [filterPersons, setFilterPersons] = useState(persons)
 
 
   const addPerson = (event) => {
@@ -49,12 +54,45 @@ const App = () => {
     setNewPhoneNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    event.preventDefault()
+    setFilterAll(event.target.value)
+  }
+
+  const handleFilterSearch = (event) => {
+    event.preventDefault()
+    // Get current filterAll state value
+    // User .filter() to filter through "persons" object list and pick out person that .include("filterALL")'s  name
+    // Thinking of using the conditional operator (ternerary function) to create a const that stores the filtered result
+    // Check if filterAll === "" when the search button is pressed
+    // IF YES THEN set peopleToShow to persons (the default unfiltered option)
+    // IF NO THEN set peopleToShow to filtered persons based on if they .include() filterAll's name
+    // This would be how I could use the ternery function
+    // Question now is what do I want my button to do once I click on it...
+    // Because I've already set peopleToShow based on filterAll's result...
+    setFilterPersons(filterAll === "" ? persons : persons.filter(person => person.name.includes(filterAll)))
+    console.log(persons)
+    console.log(filterAll)
+    console.log("This is filtedPersons: ", filterPersons)
+
+    // Not sure what I did here but it looks like it works...
+    // I basically used the setPersons state to setPersons based on filterAll using the conditional operator
+    // Bug here: filtered result seemingly deleted them... so it looks like I have to create a COPY of persons and store the filtered result in there...
+    // So even during initial setup, filterPersons have to contain a copy of persons (can't think of another way...)
+    // But how do I do that initially is the question
+    // I'm not certain if I set the initial state of a something to an object is that its own individual object or just a reference to the same point of memory to the set object...
+  }
+
+  // const peopleToShow = filterAll === ""
+  //     ? persons
+  //     : persons.filter(person => person.name.includes(filterAll))
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        Filter: <input />
-        <button type='submit'>Search</button>
+        Filter by: <input value={filterAll} onChange={handleFilterChange} />
+        <button type='submit' onClick={handleFilterSearch}>Search</button>
       </div>
       <form onSubmit={addPerson}>
         <h2>Add a Number</h2>
@@ -71,7 +109,7 @@ const App = () => {
       <h2>Numbers</h2>
       {/* Mapping persons list to a new array of list that show each individual person*/}
       <ul>
-        {persons.map(person =>
+        {filterPersons.map(person =>
           <li key={person.id}>{person.name} {person.phoneNumber}</li>
         )}
       </ul>
