3de8a7128661dcb05c9ad3a116941257f19a54d3
James Tang <52274101+ganmany5288@users.noreply.github.com>
2022-12-30 21:10:22 -0800

    Update App.js
    removed comments

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -69,15 +69,6 @@ const App = () => {
 
   const handleFilterSearch = (event) => {
     event.preventDefault()
-    // Get current filterAll state value
-    // User .filter() to filter through "persons" object list and pick out person that .include("filterALL")'s  name
-    // Thinking of using the conditional operator (ternerary function) to create a const that stores the filtered result
-    // Check if filterAll === "" when the search button is pressed
-    // IF YES THEN set peopleToShow to persons (the default unfiltered option)
-    // IF NO THEN set peopleToShow to filtered persons based on if they .include() filterAll's name
-    // This would be how I could use the ternery function
-    // Question now is what do I want my button to do once I click on it...
-    // Because I've already set peopleToShow based on filterAll's result...
     setFilterPersons(filterAll === "" ? persons : persons.filter(person => person.name.includes(filterAll)))
     console.log(persons)
     console.log(filterAll)
@@ -85,19 +76,13 @@ const App = () => {
 
     // Not sure what I did here but it looks like it works...
     // I basically used the setPersons state to setPersons based on filterAll using the conditional operator
-    // Bug here: filtered result seemingly deleted them... so it looks like I have to create a COPY of persons and store the filtered result in there...
     // So even during initial setup, filterPersons have to contain a copy of persons (can't think of another way...)
-    // But how do I do that initially is the question
     // I'm not certain if I set the initial state of a something to an object is that its own individual object or just a reference to the same point of memory to the set object...
     // Question answered: YES
 
 
 
   }
-
-  // const peopleToShow = filterAll === ""
-  //     ? persons
-  //     : persons.filter(person => person.name.includes(filterAll))
 
   return (
     <div>
