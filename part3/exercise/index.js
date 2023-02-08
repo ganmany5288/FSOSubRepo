17f33f26783ed755db95b3629fcb045d0c9f4626
James Tang <52274101+ganmany5288@users.noreply.github.com>
2023-02-08 14:08:11 -0800

    Part 3 Update
    Added comments to certain sections of code to explain is better

--- part3/exercise/index.js
+++ part3/exercise/index.js
@@ -1,10 +1,19 @@
+// Node's built-in HTTP module, primary goal is for the server creation
+import http from 'http'
 const http = require('http')
 
+// createServer is a method used to create a new web server (from http module)
+// event handler is registered to the server
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+   //  Request response (200) with content-type: text/plain
+   response.writeHead(200, { 'Content-Type': 'text/plain' })
+   // Content of the site
+   response.end('Hello World')
 })
 
+
+// Binds http server to the app variable
+// Listens to the request on PORT 3001 (if unused/available)
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
