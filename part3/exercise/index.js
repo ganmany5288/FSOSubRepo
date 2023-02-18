59084a0be7eccc37feb073d8c369fa0ed9479e1c
James Tang <rhsjamestang2019@gmail.com>
2023-02-17 22:11:00 -0800

    Part 3 exercise
    - created a small REST API that fetches hardcore resources
    - created a check to see if a note exists

--- part3/exercise/index.js
+++ part3/exercise/index.js
@@ -21,12 +21,33 @@ let notes = [
     }
 ]
 
+
+// HTTP GET request event handler
+// (request) parameter contains information on the HTTP request (in this case GET request)
+// (response) parameter contains information on how the request is responded to
+// Event handler that handles HTTP GET request made to the root (/) place in the application
+// Only gets called when it's been routed to (i.e. when address is at localhost:3001/)
 app.get('/' , (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
 
+
+// HTTP GET request, made to /api/notes address
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id) // Make sure to use the correct typing here. i.e. string != int
+    const note = notes.find(note => note.id === id)
+
+    // Checks if the specific note exist for the given ID and give the appropriate response
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
 })
 
 // Binds http server to the app variable
@@ -34,3 +55,12 @@ app.get('/api/notes', (request, response) => {
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
+
+
+// With express installed, the following is not required
+// The JSON.stringify part, its basically built into express
+// response.end(JSON.stringify(notes))
+
+
+// JSON is a string and NOT a javascript object
