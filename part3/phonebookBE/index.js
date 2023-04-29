461c53ee5912208c33a2ca722f336fd184b86094
James Tang <rhsjamestang2019@gmail.com>
2023-04-28 23:53:50 -0700

    Exercise 3.13

--- part3/phonebookBE/index.js
+++ part3/phonebookBE/index.js
@@ -2,31 +2,13 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+
+require('dotenv').config()
+const PhoneBook = require('./model/note')
+
 app.use(cors())
 app.use(express.static('build'))
 
-let people = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
 
 // How express.js uses these middleware function to handling request/response objects
 app.use(express.json())
@@ -47,9 +29,11 @@ app.use(morgan(function (tokens, req, res) {
 
 
 
-// HTTP GET request
+// HTTP GET request using MongoDB backend
 app.get('/api/people', (request, response) => {
-    response.json(people)
+    PhoneBook.find({}).then(people => {
+        response.json(people.map(people2 => people2.toJSON()))
+    })
 })
 
 
@@ -87,14 +71,12 @@ function requestLogger (request, response, next) {
 
 // app.use(requestLogger)
 
-//HTTP DELETE request
-
-// app.delete('/api/people/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     people = people.filter(p => p.id !== id)
-
-//     response.status(204).end()
-// })
+// HTTP DELETE request
+app.delete('/api/people/:id', (request, response) => {
+    const id = Number(request.params.id)
+    people = people.filter(p => p.id !== id)
+    response.status(204).end()
+})
 
 
 const generateId = () => {
