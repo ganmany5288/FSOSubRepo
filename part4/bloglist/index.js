2dab421dcc0156b60cd2088bfe9c98e82d0236fd
James Tang <52274101+ganmany5288@users.noreply.github.com>
2024-06-07 22:51:55 -0700

    Minor Refactoring

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,48 +1,12 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-
-
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://FSOtest:Kekwing@fullstackopencluster.phlc1en.mongodb.net/?retryWrites=true&w=majority&appName=FullStackOpenCluster'
-
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog 
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-
+const server = http.createServer(app)
 
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+// console.log("PORT Number: ", PORT)
+let MONGODB_URI = process.env.MONGO_URI_DEV
+// console.log("MONGODB URI", MONGODB_URI)
 
+// if (process.env.NODE_ENV === 'test'){
+//     MONGODB_URI = process.env.MONGODB_URI_TEST
+// } else if (process.env.NODE_ENV === 'development'){
+//     MONGODB_URI = process.env.MONGODB_URI_DEV
+// }
 
 module.exports = {
     MONGODB_URI,
